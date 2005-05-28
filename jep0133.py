436d4e179428d6a5d039d3dd93be2fd5dcd2044a
Mike Albon <mikea@yuri.org.uk>
2005-05-28 09:34:13 +0000

    Lots of bug fixes -- thanks Norman

--- jep0133.py
+++ jep0133.py
@@ -1,8 +1,10 @@
+
 # Service administration commands Jep-0133 for the xmpppy based transports written by Mike Albon
 import xmpp
 from xmpp.protocol import *
 import xmpp.commands
 
+administrators = []
 
 """This file is the JEP-0133 commands I feel are applicable to the transports.
 
@@ -19,6 +21,7 @@ NS_ADMIN = 'http://jabber.org/protocol/admin'
 NS_ADMIN_ACTIVE_USERS = NS_ADMIN+'#get-active-users'
 NS_ADMIN_REGISTERED_USERS = NS_ADMIN+'#get-registered-users'
 NS_ADMIN_EDIT_ADMIN = NS_ADMIN+'#edit-admin'
+NS_COMMAND = 'http://jabber.org/protocol/commands'
 
 class Active_Users_Command(xmpp.commands.Command_Handler_Prototype):
 	"""This is the active users command as documented in section 4.12  of JEP-0133.
@@ -37,12 +40,12 @@ class Active_Users_Command(xmpp.commands.Command_Handler_Prototype):
 	def cmdFirstStage(self,conn,request):
 		"""Build the reply to complete the request"""
 		if request.getFrom().getStripped() in administrators:
-		    reply = request.buildReply()
-		    form = DataForm(typ='result',data=[DataField(typ='hidden',name='FORM_TYPE',value=NS_ADMIN),DataField(label='The list of active users',name='activeuserjids',value=self.transport.userlist.keys(),typ='jid-multi')])
+		    reply = request.buildReply('result')
+		    form = DataForm(typ='result',data=[DataField(typ='hidden',name='FORM_TYPE',value=NS_ADMIN),DataField(desc='The list of active users',name='activeuserjids',value=self.transport.users.keys(),typ='jid-multi')])
         	    reply.addChild(name='command',attrs={'xmlns':NS_COMMAND,'node':request.getTagAttr('command','node'),'sessionid':self.getSessionID(),'status':'completed'},payload=form)
         	    self.jabber.send(reply)
         	else:
-        	    self.jabber.send(Error(ERR_FORBIDDEN,request))
+        	    self.jabber.send(Error(request,ERR_FORBIDDEN))
         	raise NodeProcessed
         	
 class Registered_Users_Command(xmpp.commands.Command_Handler_Prototype):
@@ -62,12 +65,12 @@ class Registered_Users_Command(xmpp.commands.Command_Handler_Prototype):
 	def cmdFirstStage(self,conn,request):
 		"""Build the reply to complete the request"""
 		if request.getFrom().getStripped() in administrators:
-		    reply = request.buildReply()
-		    form = DataForm(typ='result',data=[DataField(typ='hidden',name='FORM_TYPE',value=NS_ADMIN),DataField(label='The list of registered users',name='registereduserjids',value=self.transport.userfile.keys(),typ='jid-multi')])
+		    reply = request.buildReply('result')
+		    form = DataForm(typ='result',data=[DataField(typ='hidden',name='FORM_TYPE',value=NS_ADMIN),DataField(desc='The list of registered users',name='registereduserjids',value=self.transport.userfile.keys(),typ='jid-multi')])
         	    reply.addChild(name='command',attrs={'xmlns':NS_COMMAND,'node':request.getTagAttr('command','node'),'sessionid':self.getSessionID(),'status':'completed'},payload=form)
         	    self.jabber.send(reply)
         	else:
-        	    self.jabber.send(Error(ERR_FORBIDDEN,request))
+        	    self.jabber.send(Error(request,ERR_FORBIDDEN))
         	raise NodeProcessed
 
 
@@ -80,9 +83,9 @@ class Edit_Admin_List_Command(xmpp.commands.Command_Handler_Prototype):
 	def __init__(self,transport,jabber,configfile, configfilename, administrators):
 		"""Initialise the command object"""
 		xmpp.commands.Command_Handler_Prototype.__init__(self)
-		self.inital = {'execute':self.cmdFirstStage }
+		self.initial = {'execute':self.cmdFirstStage }
 		self.transport = transport
-		self.jaber = jabber
+		self.jabber = jabber
 		self.configfile = configfile
 		self.configfilename = configfilename
 		self.administrators = administrators
@@ -92,22 +95,22 @@ class Edit_Admin_List_Command(xmpp.commands.Command_Handler_Prototype):
 		if request.getFrom().getStripped() in administrators:
 		   # Setup session ready for form reply
 		   session = self.getSessionID()
-		   sessions[session] = {'jid':request.getFrom(),'actions':{'cancel':self.cmdCancel,'next':self.cmdSecondStage}}
+		   self.sessions[session] = {'jid':request.getFrom(),'actions':{'cancel':self.cmdCancel,'next':self.cmdSecondStage}}
 		   # Setup form with existing data in
 		   reply = request.buildReply('result')
-		   form = DataForm(title='Editing the Admin List',data=['Fill out this form to edit the list of entities who have administrative privileges', DataField(typ='hidden',name='FORM_TYPE',value=NS_ADMIN),DataField(label='The Admin List', typ='jid-multi', name='adminjids',value=self.administrators)])
+		   form = DataForm(title='Editing the Admin List',data=['Fill out this form to edit the list of entities who have administrative privileges', DataField(typ='hidden',name='FORM_TYPE',value=NS_ADMIN),DataField(desc='The Admin List', typ='jid-multi', name='adminjids',value=self.administrators)])
 		   replypayload = [Node('actions',attrs={'execute':'next'},payload=[Node('next')]),form]
 		   reply.addChild(name='command',attrs={'xmlns':NS_COMMAND,'node':request.getTagAttr('command','node'),'sessionid':session,'status':'executing'},payload=replypayload)
 		   self.jabber.send(reply)
 		else:
-		   self.jabber.send(Error(ERR_FORBIDDEN,request))
+		   self.jabber.send(Error(request,ERR_FORBIDDEN))
 		raise NodeProcessed
 		   
 	def cmdSecondStage(self,conn,request):
 		"""Apply and save the config"""
 		form = DataForm(node=result.getTag(name='command').getTag(namespace=NS_DATA))
-		if sessions.has_key(request.getTagAttr('command','sessionid')):
-			if sessions[request.getTagAttr('command','sessionid')]['jid'] == request.getFrom():
+		if self.sessions.has_key(request.getTagAttr('command','sessionid')):
+			if self.sessions[request.getTagAttr('command','sessionid')]['jid'] == request.getFrom():
 				self.administrators = form.getField('adminjids').getValues()
 				adminstr = ''
 				for each in self.administrators:
@@ -120,6 +123,13 @@ class Edit_Admin_List_Command(xmpp.commands.Command_Handler_Prototype):
 				reply.addChild(name='command',attrs={'xmlns':NS_COMMAND,'node':request.getTagAttr('command','node'),'sessionid':session,'status':'completed'})
 				self.jabber.send(reply)
 			else:
-				self.jabber.send(Error(ERR_BAD_REQUEST,request))
+				self.jabber.send(Error(request,ERR_BAD_REQUEST))
 		else:
-			self.jabber.send(Error(ERR_BAD_REQUEST,request))   
+			self.jabber.send(Error(request,ERR_BAD_REQUEST))   
+
+	def cmdCancel(self,conn,request):
+		self.sessions.remove(self.getSessionID())
+		reply = requst.buildReply('result')
+		reply.addChild(name='command',attrs={'xmlns':NS_COMMAND,'node':request.getTagAttr('command','node'),'sessionid':session,'status':'canceled'})
+		self.jabber.send(reply)
+		raise NodeProcessed
