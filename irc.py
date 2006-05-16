fe6b6a58e07f1906016d9917d0fac83c687070ae
Norman Rasmussen <norman@rasmussen.co.za>
2006-05-16 12:16:13 +0000

    Fixed bad loop creating material

--- irc.py
+++ irc.py
@@ -585,10 +585,11 @@ class Transport:
                 elif server and userfile[fromstripped].has_key('servers') and userfile[fromstripped]['servers'].has_key(server):
                     conf = userfile[fromstripped]['servers'][server]
             if not conf:
-                if type != 'unsubscribed':
+                if type != 'unsubscribed' and type != 'error':
                     self.jabber.send(Presence(to=fromjid, frm=to, typ = 'unsubscribe'))
                     self.jabber.send(Presence(to=fromjid, frm=to, typ = 'unsubscribed'))
-                self.jabber.send(Error(event,ERR_BAD_REQUEST))
+                    if type != 'unsubscribe':
+		        self.jabber.send(Error(event,ERR_BAD_REQUEST))
                 return
             if type == 'subscribe':
                 self.jabber.send(Presence(to=fromjid, frm = to, typ = 'subscribe'))
@@ -655,10 +656,11 @@ class Transport:
             if server and userfile.has_key(fromstripped) and userfile[fromstripped].has_key('servers') and userfile[fromstripped]['servers'].has_key(server):
                 conf = userfile[fromstripped]['servers'][server]
             if not conf:
-                if type != 'unsubscribed':
+                if type != 'unsubscribed' and type != 'error':
                     self.jabber.send(Presence(to=fromjid, frm=to, typ = 'unsubscribe'))
                     self.jabber.send(Presence(to=fromjid, frm=to, typ = 'unsubscribed'))
-                self.jabber.send(Error(event,ERR_BAD_REQUEST))
+		    if type != 'unsubscribe':
+                        self.jabber.send(Error(event,ERR_BAD_REQUEST))
                 return
             subscriptions = []
             if conf.has_key('subscriptions'):
