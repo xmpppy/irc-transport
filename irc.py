9dc0c2f02e1765f8a51737016b8e48537500e995
Norman Rasmussen <norman@rasmussen.co.za>
2009-10-06 15:32:12 -0700

    Use latest git revision as version information

--- irc.py
+++ irc.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
-# $Id$
-version = 'CVS ' + '$Revision$'.split()[1]
+import os
+try:
+    version = 'git ' + open(os.path.join(os.path.dirname(__file__), '.git/refs/heads/master')).read().strip()
+except:
+    version = 'unknown'
 #
 # IRC transport
 # January 2004 Copyright (c) Mike Albon
